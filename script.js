53e4c281ae1a0dea27b35fb155cb101e14c5fdf7
tanselbay1 <tanselbay1@hotmail.com>
2021-08-16 15:21:06 +0700

    Logic of the app finished

---
+++ script.js
@@ -0,0 +1,31 @@
+async function getWeather(location, units) {
+  try {
+    // For Celcius use 'metric' and for Fahrenheit, 'imperial'
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&APPID=298f0108207d823442375553239e0577`
+    );
+    const data = await response.json();
+    console.log(data.weather[0].main);
+    console.log(data.main.temp);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// getWeather("Bangkok", "metric");
+
+// function getUserInput(location, units) {
+//    document
+// }
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formEl = document.forms.weatherForm;
+  const formData = new FormData(formEl);
+  const locationData = formData.get("location");
+  const unitsData = formData.get("units");
+  getWeather(locationData, unitsData);
+  console.log(locationData);
+  console.log(unitsData);
+});
